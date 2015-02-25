88d18890da9a98da7c4547c22dca407719737ba2
Alex McConnell <adm3333@yahoo.com>
2015-02-25 17:45:48 -0500

    song_suggestions view returns results

--- app/controllers/song_suggestions_controller.rb
+++ app/controllers/song_suggestions_controller.rb
@@ -2,15 +2,11 @@ class SongSuggestionsController < ApplicationController
   def show
     @results = nil
     if params[:q]
-      tracks = RSpotify::Track.search(params[:q], limit: 20, offset: 0, market: 'US')
-      binding.pry
-      @results = JSON.parse(tracks.body)['results']
+      @tracks = RSpotify::Track.search(params[:q], limit: 20, offset: 0, market: 'US')
     end
-    render :show
   end
 
   def index
     @songsuggestion = SongSuggestion.all
-    render :index
   end
 end
