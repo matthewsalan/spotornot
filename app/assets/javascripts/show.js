9b44292bbf79e23cdd51856f6e44cd1dc53f4170
Alex McConnell <adm3333@yahoo.com>
2015-02-26 10:28:40 -0500

    preview function working with jquery

---
+++ app/assets/javascripts/show.js
@@ -0,0 +1,11 @@
+jQuery(document).ready(function($) {
+  var lastid;
+  $(".video").click(function (e) {
+    e.preventDefault();
+    var url = this.href;
+    $("#" + lastid).css("background", "white")
+    lastid = $(this).parent().parent().attr('id');
+    $("#" + lastid).css("background", "#E0E0E0")
+    $("#preview").html('<audio autoplay controls><source src="' + url + '" type="audio/mpeg"></audio>');
+  });
+});
