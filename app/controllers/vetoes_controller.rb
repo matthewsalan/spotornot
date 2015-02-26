c7e0cde059d0304db0f25c1ba94f96b391adf927
Alan Matthews <matthewsa1320@gmail.com>
2015-02-26 16:46:49 -0500

    removing veto and vote button for users who have already voted and or vetoed

--- app/controllers/vetoes_controller.rb
+++ app/controllers/vetoes_controller.rb
@@ -1,10 +1,10 @@
-class VetoesControllerController < ApplicationController
-  def veto
+class VetoesController < ApplicationController
+  def create
     @song = SongSuggestion.find(params[:id])
     @week = Playlist.last
-    used_veto? = current_user.votes.find_by(veto: true)
+    used_veto = current_user.votes.find_by(veto: true)
     veto = Vote.new
-    unless used_veto?  
+    unless used_veto  
       veto.veto = true
       veto.user_id = current_user.id
       veto.song_suggestion_id = @song.id
@@ -13,7 +13,7 @@ class VetoesControllerController < ApplicationController
       flash[:alert] = "You have already used your veto"
     end
     veto.save
-    redirect_to song_suggestion_index
+    redirect_to song_suggestions_index_path
   end
 end
 
