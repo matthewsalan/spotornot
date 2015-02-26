d1bf84938c5a12b00c667ba0cefdd6a18c55f951
Alex McConnell <adm3333@yahoo.com>
2015-02-25 20:30:28 -0500

    creation works and stops people from making a second

--- app/controllers/song_suggestions_controller.rb
+++ app/controllers/song_suggestions_controller.rb
@@ -12,10 +12,18 @@ class SongSuggestionsController < ApplicationController
   end
 
   def create
-    @track = RSpotify::Track.find(params[:id])
-    @song = SongSuggestion.new(user_id: current_user.id, artist: @track.artists.first.name,
-                               title: @track.name, spotify_id: params[:id])
-    binding.pry
+    @this_week = Playlist.last
+    @playlist = SongSuggestion.where(spotify_id: params[:id],
+                created_at: @this_week.created_at..@this_week.created_at + 1.weeks)
+    if @playlist.size < 1
+      @track = RSpotify::Track.find(params[:id])
+      @suggestion = SongSuggestion.create(user_id: current_user.id, artist: @track.artists.first.name,
+                                 title: @track.name, spotify_id: params[:id])
+      flash[:notice] = "The song #{@track.name} has been added to this week's suggestions."
+    else
+      flash[:alert] = "Sorry, #{@playlist.first.title} was already suggested this week."
+    end
+    redirect_to root_path
   end
 
   def destroy
