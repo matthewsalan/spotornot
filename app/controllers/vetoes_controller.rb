1b1f31579ac91786899f1f4f677ee8aeb0103302
Alan Matthews <matthewsa1320@gmail.com>
2015-02-26 19:12:40 -0500

    Brit refactoring on vetoes controller and indention fix on index.html.erb

--- app/controllers/vetoes_controller.rb
+++ app/controllers/vetoes_controller.rb
@@ -1,27 +1,21 @@
 class VetoesController < ApplicationController
+  before_action :authenticate_user!
+
   def create
     @song = SongSuggestion.find(params[:id])
     @week = Playlist.last
     used_veto = current_user.votes.find_by(veto: true)
-    veto = Vote.new
-    unless used_veto  
-      veto.veto = true
-      veto.user_id = current_user.id
-      veto.song_suggestion_id = @song.id
-      flash[:notice] = "You have used your veto"
-    else
+    @vote = Vote.new
+    if used_veto  
       flash[:alert] = "You have already used your veto"
+    else
+      @vote.veto = true
+      @vote.user_id = current_user.id
+      @vote.song_suggestion_id = @song.id
+      @vote.save
+      flash[:notice] = "You have used your veto"
     end
-    veto.save
     redirect_to song_suggestions_index_path
   end
 end
-
-
-
-
-
-
-
-
 
